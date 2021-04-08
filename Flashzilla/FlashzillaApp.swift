cee2e31f1f058ed20d73718651ef09add68a4b4e
RajMalHotRA123 <RM_frankfurt@gmx.de>
2021-04-08 16:47:54 +0200

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Raj Malhotra on 08.04.21.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
