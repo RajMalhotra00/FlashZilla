32b60bc41f170bcaa28f8799ae2cac4c8ccc88f6
RajMalHotRA123 <RM_frankfurt@gmx.de>
2021-04-12 18:25:06 +0200

    Added a timer, start again Button, user friendliness for people with red green weakness and haptics for success and failure case

--- CardView.swift
+++ CardView.swift
@@ -12,11 +12,25 @@ struct CardView: View {
     var removal: (() -> Void)? = nil
     @State private var offset = CGSize.zero
     @State private var isShowingAnswer = false
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    @State private var feedback = UINotificationFeedbackGenerator()
     var body: some View {
         ZStack{
             RoundedRectangle(cornerRadius: 25, style: .continuous)
-                .fill(Color.white)
-                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                .fill(
+                    differentiateWithoutColor
+                        ? Color.white
+                        : Color.white
+                            .opacity(1 - Double(abs(offset.width / 50)))
+
+                )
+                .background(
+                    differentiateWithoutColor
+                        ? nil
+                        : RoundedRectangle(cornerRadius: 25, style: .continuous)
+                            .fill(offset.width > 0 ? Color.green : Color.red)
+                )
+                .shadow(radius: 10)
             
             VStack{
                 Text(card.prompt)
@@ -38,11 +52,18 @@ struct CardView: View {
         .rotationEffect(.degrees(Double(offset.width / 5)))
         .gesture(
             DragGesture()
-                .onChanged{ gesture in
-                    self.offset = gesture.translation
+                .onChanged{ offset in
+                    self.offset = offset.translation
+                    self.feedback.prepare()
                 }
                 .onEnded{ _ in
                     if abs(self.offset.width) > 100 {
+                        if self.offset.width > 0 {
+                            //shouldn't be used too much. Failure Haptic should be kept and Success Haptic removed for better user experience 
+                            self.feedback.notificationOccurred(.success)
+                        } else {
+                            self.feedback.notificationOccurred(.error)
+                        }
                         self.removal?()
                     } else{
                         self.offset = .zero
--- Flashzilla/Card.swift
+++ Flashzilla/Card.swift
@@ -10,7 +10,6 @@ import SwiftUI
 struct Card {
     let prompt: String
     let answer: String
-    
     static var example: Card {
         Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
     }
